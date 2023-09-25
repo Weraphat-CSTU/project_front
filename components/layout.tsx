7b218125c3d53ad3a1ad883d8d9ec835fcf59f55
worawut <worawut48@hotmail.co.th>
2023-09-25 10:53:01 +0700

    add prettier

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,57 +1,57 @@
-import Image from "next/image";
-import React, { type FC } from "react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import Index from "@/pages";
+import Image from 'next/image';
+import React, { type FC } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Index from '@/pages';
 type props = {
-  children?: React.ReactNode;
+    children?: React.ReactNode;
 };
 
-const Layout: FC<props> = ({ children }) => {  
-  const router = useRouter();
+const Layout: FC<props> = ({ children }) => {
+    const router = useRouter();
 
-  const Navigator = [
-    { path: "/scholarship", lebel: "ทุนการศึกษา" },
-    { path: "/historyscholarship", lebel: "ข้อมูลผู้ใช้" },
-    { path: "/", lebel: "ออกจากระบบ" }
-  ];     
-  return (
-    <div>
-      <div className="w-full h-[60px] bg-[#EB9D48] flex items-center">
-        <div className="mx-auto max-w-3xl lg:max-w-7xl w-full h-full ">
-          <div className="flex justify-between">
-            <div className="flex items-center">
-              <Image
-                src="/qjg3jmr9-removebg-preview.png"
-                width={50}
-                height={10}
-                alt="Picture of the author"
-              />
-              <div className="font-bold text-xl text-white ">CsScholarship</div>
+    const Navigator = [
+        { path: '/scholarship', lebel: 'ทุนการศึกษา' },
+        { path: '/historyscholarship', lebel: 'ข้อมูลผู้ใช้' },
+        { path: '/', lebel: 'ออกจากระบบ' },
+    ];
+    return (
+        <div>
+            <div className="w-full h-[60px] bg-[#EB9D48] flex items-center">
+                <div className="mx-auto max-w-3xl lg:max-w-7xl w-full h-full ">
+                    <div className="flex justify-between">
+                        <div className="flex items-center">
+                            <Image
+                                src="/qjg3jmr9-removebg-preview.png"
+                                width={50}
+                                height={10}
+                                alt="Picture of the author"
+                            />
+                            <div className="font-bold text-xl text-white ">CsScholarship</div>
+                        </div>
+                        <div className="flex items-center space-x-10">
+                            {Navigator.map((item, Index) => {
+                                return (
+                                    <div key={Index}>
+                                        <Link
+                                            href={item.path}
+                                            className={
+                                                item.path === router.asPath
+                                                    ? 'font-bold text-lg text-white cursor-pointer underline'
+                                                    : 'font-bold text-lg text-white cursor-pointer '
+                                            }
+                                        >
+                                            {item.lebel}
+                                        </Link>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="flex items-center space-x-10">
-              {Navigator.map((item, Index) => {
-                return (
-                  <div key={Index}>
-                  <Link
-                    href={item.path}
-                    className={
-                      item.path === router.asPath
-                        ? "font-bold text-lg text-white cursor-pointer underline"
-                        : "font-bold text-lg text-white cursor-pointer "
-                    }
-                  >
-                    {item.lebel}
-                  </Link>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+            <div>{children}</div>
         </div>
-      </div>
-      <div>{children}</div>
-    </div>
-  );
+    );
 };
 export default Layout;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}','node_modules/daisyui/dist/**/*.js', 
+    "./src/**/*.{js,ts,jsx,tsx}",
     'node_modules/react-daisyui/dist/**/*.js'
   ],
   theme: {
